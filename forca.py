ac6fb4e8a0cf117f3b2ddd787ed9280b99e8aed2
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-24 15:47:12 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,32 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo de forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+        # para tirar os espaço do início e do final do que for digitado no input
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}.".format(letra, index))
+
+            index = index + 1
+
+        print("Jogando...")
+
+    print("Fim do jogo!")
+
+# se executar o forca como programa principal a
+# variável name recebe o valor main
+if(__name__ == "__main__"):
+    jogar()
